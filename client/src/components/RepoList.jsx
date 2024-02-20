32a02f7bba4f888331517fb04044f769598d3c33
Flora Zhang <Azurstyle@outlook.com>
2024-02-20 16:56:41 -0500

    implement Repolist to render the server data

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -4,6 +4,14 @@ const RepoList = ({ repos }) => (
   <div>
     <h4> Repo List Component </h4>
     There are {repos.length} repos.
+    <ul>
+      {repos.map(repo => (
+        <li key={repo.html_url}>
+         <a href={repo.html_url}>{repo.name}</a>
+         {'- Stars:'}{repo.stargazers_count}
+         </li>
+      ))}
+    </ul>
   </div>
 )
 
